50d6c43a7e167611de16c56343dfb925633db98e
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2014-04-10 11:04:16 -0700

    Added lib versions, option to run tests using device ids

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ CLOUD_ENDPOINT='https://cloud.testdroid.com'
 
 
 VCR.configure do |c|
+	puts "init VCR"
   c.cassette_library_dir = File.join(File.dirname(__FILE__), 'fixtures', 'cassettes')
   c.hook_into :webmock , :faraday
   c.default_cassette_options = {:serialize_with => :json,    :preserve_exact_body_bytes => true, :decode_compressed_response => true}
@@ -21,6 +22,12 @@ end
 def client 
 	@client ||= begin
 		client = Client.new('defaultuser', 'password')
+	rescue Exception => e
+	end
+end
+def client_local_host 
+	@client_local_host ||= begin
+		client_local_host = Client.new('admin@localhost', 'admin', 'http://localhost:8080/testdroid-cloud')
 	rescue Exception => e
 	end
 end
