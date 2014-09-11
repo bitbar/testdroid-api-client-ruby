008178d097cf7eeb94566fdba00978be7691e390
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2014-09-10 17:49:58 -0700

    Added device sessions

--- lib/testdroid_api/device_sessions.rb
+++ lib/testdroid_api/device_sessions.rb
@@ -6,6 +6,10 @@ module TestdroidAPI
 			def	initialize(uri, client, params= {})
 				super uri, client,"deviceSession", params
 				@uri, @client = uri, client
-			end  
+			end
+      def release
+          resp = @client.post("#{@uri}/release", params= {})
+          DeviceSession.new("#{@uri}", @client, resp)
+      end  
 		end
 end
--- lib/testdroid_api/user.rb
+++ lib/testdroid_api/user.rb
@@ -2,7 +2,7 @@ module TestdroidAPI
 	class User < CloudResource
 		def initialize(uri, client, params={})
 			super uri, client,"users", params
-			sub_items :projects, :device_groups
+			sub_items :projects, :device_groups, :device_sessions
 		end
   	end
 end
